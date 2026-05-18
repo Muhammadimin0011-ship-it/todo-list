be16c2d4c96d0ab16027a75ff44d39a19d017531
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-05-18 21:00:19 +0500

    html.css

---
+++ script.js
@@ -0,0 +1,46 @@
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+
+function addCard() {
+
+    if (input.value === "") {
+        return;
+    }
+
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+
+    let text = document.createElement("p");
+    text.textContent = input.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+
+
+    deleteBtn.addEventListener("click", function () {
+        card.remove();
+    });
+
+
+    card.appendChild(text);
+    card.appendChild(deleteBtn);
+
+
+    output.appendChild(card);
+
+    input.value = "";
+}
+
+
+btn.addEventListener("click", addCard);
+
+input.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        addCard();
+    }
+
+});
